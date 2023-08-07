0efc37b809a0b67dcb316e7acdee685b97bbf7d1
CIIPres <70212206+CIIPres@users.noreply.github.com>
2023-08-07 14:15:07 -0400

    Update Get-LenovoWarranty.ps1
    Updated to scrape directly from Lenovo's webpage.

--- private/Get-LenovoWarranty.ps1
+++ private/Get-LenovoWarranty.ps1
@@ -1,30 +1,41 @@
+
 function get-LenovoWarranty([Parameter(Mandatory = $true)]$SourceDevice, $client) {
     $today = Get-Date -Format yyyy-MM-dd
-    $APIURL = "https://warrantyapiproxy.azurewebsites.net/api/Lenovo?Serial=$SourceDevice"
+    $APIURL = "https://pcsupport.lenovo.com/us/en/api/v4/mse/getproducts?productId=$SourceDevice"
     $Req = Invoke-RestMethod -Uri $APIURL -Method get
-    if ($req.Warproduct) {
-        $warlatest = $Req.EndDate | ForEach-Object { [datetime]$_ } | sort-object | select-object -last 1 
-        $WarrantyState = if ($warlatest -le $today) { "Expired" } else { "OK" }
+    
+    if($req.id){
+        $APIURL = "https://pcsupport.lenovo.com/us/en/products/$($req.id)/warranty"
+        $Req = Invoke-RestMethod -Uri $APIURL -Method get
+        $search = $Req |Select-String -Pattern "var ds_warranties = window.ds_warranties \|\| (.*);[\r\n]*"
+        $jsonWarranties = $search.matches.groups[1].value |ConvertFrom-Json
+        }
+
+    if ($jsonWarranties.BaseWarranties) {
+        $warfirst = $jsonWarranties.BaseWarranties |sort-object -property [DateTime]End |select-object -first 1
+        $warlatest = $jsonWarranties.BaseWarranties |sort-object -property [DateTime]End |select-object -last 1
         $WarObj = [PSCustomObject]@{
-            'Serial'                = $Req.Serial
-            'Warranty Product name' = $Req.WarProduct
-            'StartDate'             = [DateTime]($Req.StartDate)
-            'EndDate'               = [DateTime]($Req.EndDate)
-            'Warranty Status'       = $WarrantyState
-            'Client'                = $Client
+            'Serial' = $jsonWarranties.Serial
+            'Warranty Product name' = $jsonWarranties.ProductName
+            'StartDate' = [DateTime]($warfirst.Start)
+            'EndDate' = [DateTime]($warlatest.End)
+            'Warranty Status' = $warlatest.StatusV2
+            'Client' = $Client
+            'Product Image' = $jsonWarranties.ProductImage
+            'Warranty URL' = $jsonWarranties.WarrantyUpgradeURLInfo.WarrantyURL
         }
     }
     else {
         $WarObj = [PSCustomObject]@{
-            'Serial'                = $SourceDevice
+            'Serial' = $SourceDevice
             'Warranty Product name' = 'Could not get warranty information'
-            'StartDate'             = $null
-            'EndDate'               = $null
-            'Warranty Status'       = 'Could not get warranty information'
-            'Client'                = $Client
+            'StartDate' = $null
+            'EndDate' = $null
+            'Warranty Status' = 'Could not get warranty information'
+            'Client' = $Client
+            'Product Image' = ""
+            'Warranty URL' = ""
         }
     }
     return $WarObj
- 
- 
 }
